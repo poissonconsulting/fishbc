47e6335a3ccd08b938038873bacad9c31671a3c9
Sarah Lyons <sarah@poissonconsulting.ca>
2026-07-14 09:40:35 -0700

    update build script

--- scripts/build.R
+++ scripts/build.R
@@ -1,13 +1,10 @@
 roxygen2md::roxygen2md()
 
-styler::style_pkg(
-  scope = "line_breaks",
-  filetype = c("R", "Rmd")
-)
-
-lintr::lint_package(linters = lintr::linters_with_defaults(
-  line_length_linter = lintr::line_length_linter(1000),
-  object_name_linter = lintr::object_name_linter(regexes = ".*"))
+lintr::lint_package(
+  linters = lintr::linters_with_defaults(
+    line_length_linter = lintr::line_length_linter(1000),
+    object_name_linter = lintr::object_name_linter(regexes = ".*")
+  )
 )
 
 lintr::lint_package()
@@ -20,3 +17,15 @@ pkgdown::build_reference()
 pkgdown::build_site()
 
 devtools::check()
+
+# Test files that have less then 100% coverage
+covr:::tally_coverage(covr::package_coverage()) |>
+  dplyr::summarize(
+    percent_coverage = mean(value > 0) * 100,
+    .by = filename
+  ) |>
+  dplyr::filter(percent_coverage < 100) |>
+  dplyr::arrange(percent_coverage)
+
+# Report for all test files
+covr::report(covr::package_coverage())
